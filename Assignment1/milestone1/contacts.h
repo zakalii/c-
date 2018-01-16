5bd0293722a89ad08b507f1c71a2bf65e2e2d250
zakalii <35506610+zakalii@users.noreply.github.com>
2018-01-16 16:03:00 -0500

    Create contacts.h

---
+++ Assignment1/milestone1/contacts.h
@@ -0,0 +1,35 @@
+/* -------------------------------------------
+Name: 
+Student number: 
+Email:
+Section: 
+Date: 
+----------------------------------------------
+Assignment: 1
+Milestone:  1
+---------------------------------------------- */
+
+// Structure type Name declaration
+struct Name {
+    char firstName[31];
+    char middleInitial[7];
+    char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code here...
+struct Address {
+	int streetNumber;
+	char street[41];
+	int apartmentNumber;
+	char postalCode[8];
+	char city[41];
+};
+
+// Structure type Numbers declaration
+// Place your code here...
+struct Numbers {
+	char cell[21];
+	char home[21];
+	char business[21];
+};
